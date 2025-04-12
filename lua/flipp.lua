90d47254739ceb2f84075c15f72f86d47f8b309e
Will Trojniak <william.trojniak@gmail.com>
2025-04-12 18:59:51 -0400

    fix\!: customizable mappings for multiple file pairs

--- lua/flipp.lua
+++ lua/flipp.lua
@@ -1,89 +1,64 @@
 local M = {}
 
----@class flipp.Extensions
----@field filetype string: Pattern to match the filetype agains
----@field header string[]: Patterns to match against for header files
----@field source string[]: Patterns to match against for source files
-
 ---@class flipp.Opts
----@field extensions flipp.Extensions
-
+---@field extensions {[string]:string[]}
 
 ---@type flipp.Opts
 local options = {
   extensions = {
-    filetype = "{c,cpp}",
-    header = { "h", "hpp" },
-    source = { "c", "cpp" },
+    h = { "c", "cpp" },
+    hpp = { "c", "cpp" },
+    c = { "h", "hpp" },
+    cpp = { "h", "hpp" },
   }
 }
 
 ---@param opts flipp.Opts|nil: opts
 M.setup = function(opts)
-  --HACK: This should more fully extend extensions
   opts = opts or options
   opts.extensions = opts.extensions or options.extensions
 
-  vim.api.nvim_create_autocmd({ "FileType", "BufReadPost" }, {
-    pattern = opts.extensions.filetype,
+  vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
     callback = function(ev)
+      local ext = vim.fn.expand("%:e")
+      local targets = opts.extensions[ext]
+      if not targets then return end
+
       vim.api.nvim_buf_create_user_command(0, 'Flipp', function(_)
-          M.swap(opts.extensions)
+          M.swap(targets)
         end,
         { nargs = 0 })
 
       -- FIXME: Allow for dynamic keymaps
-      vim.keymap.set("n", "<leader>go", function() M.swap(opts.extensions) end,
+      vim.keymap.set("n", "<leader>go", function() M.swap(targets) end,
         { buffer = ev.buf, desc = "go to source/header file" })
     end
   })
 end
 
 
---- @param extensions flipp.Extensions
+--- @param extensions string[]: target extensions
 M.swap = function(extensions)
-  local buf_name = vim.api.nvim_buf_get_name(0)
-  local base_name = vim.fs.basename(buf_name)
-  local dir_name = vim.fs.dirname(buf_name)
-
-  local ext_index = string.find(base_name, ".", 0, true) or string.len(base_name)
-  local file_name = string.sub(base_name, 0, ext_index)
-  local file_ext = string.sub(base_name, ext_index + 1)
-
-  ---@type string[]|nil
-  local exs = nil
-  if vim.list_contains(extensions.header, file_ext) then
-    exs = extensions.source
-  elseif vim.list_contains(extensions.source, file_ext) then
-    exs = extensions.header
-  else
-    return
-  end
+  local dir = vim.fn.expand("%:p:h")
+  local file = vim.fn.expand("%:t:r")
 
   ---@type string[]
   local targets = {}
-  for _, e in ipairs(exs) do
-    table.insert(targets, file_name .. e)
+  for _, e in ipairs(extensions) do
+    table.insert(targets, file .. "." .. e)
   end
 
 
   local found = vim.fs.find(
     targets,
-    { limit = math.huge, type = "file", path = dir_name })
+    { limit = math.huge, type = "file", path = dir })
 
-
-  ---@type string|nil
-  local t
-  for _, file in ipairs(found) do
-    t = file
-    break
-  end
-
-  if t == nil then return end
+  if #found == 0 then return end
+  local t = found[1]
 
   -- TODO: Check if file is already open in a buffer and open that buffer to current window
   -- TODO: Check if buffer is already open in another window / tab and change focus to that window/tab
-  vim.cmd(string.format("%s %s", "edit", vim.fn.fnameescape(t)))
+  pcall(vim.cmd(string.format("%s %s", "edit", vim.fn.fnameescape(t))))
 end
 
 M.setup()
