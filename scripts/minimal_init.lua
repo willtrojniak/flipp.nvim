67074c7e8c2185670680e1e1bb969477a37285df
Will Trojniak <william.trojniak@gmail.com>
2025-06-07 21:13:56 -0400

    fix: 🔧 fix treesitter setup for CI

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -2,6 +2,14 @@ vim.opt.runtimepath:append(vim.fn.getcwd())
 vim.opt.runtimepath:append("../plenary.nvim/")
 vim.opt.runtimepath:append("../nvim-treesitter/")
 vim.cmd("runtime! plugin/plenary.vim")
+vim.cmd("runtime! plugin/nvim-treesitter.lua")
 
 local nvim_ts = require('nvim-treesitter.configs')
-nvim_ts.setup({ ensure_installed = { "cpp" } })
+nvim_ts.setup({
+  sync_install = true,
+  ensure_installed = { "cpp" },
+  ignore_install = {},
+  auto_install = false,
+
+  modules = {},
+})
